2c3b112a72c32d72652e16b2f76538688a811a2f
Matthias Zeleny <matthias.zeleny@andrena.de>
2018-10-24 13:12:32 +0200

    extract dot movement the inner view

--- app/src/main/java/comandrenamatthiasz/httpsgithub/stayinside/GameView.kt
+++ app/src/main/java/comandrenamatthiasz/httpsgithub/stayinside/GameView.kt
@@ -1,27 +1,85 @@
 package layout
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
+import java.util.*
+import kotlin.concurrent.schedule
 
-class GameView: View {
+class GameView : View {
 
 
-    constructor(context: Context):super(context){
+    constructor(context: Context) : super(context) {
         init()
     }
 
-    constructor(context: Context,attributeSet: AttributeSet):super(context,attributeSet){
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
         init()
     }
 
-    constructor(context: Context,attributeSet: AttributeSet, defStyle: Int):super(context,attributeSet,defStyle){
+    constructor(context: Context, attributeSet: AttributeSet, defStyle: Int) : super(context, attributeSet, defStyle) {
         init()
     }
 
+    private var xVelocity = -10f
+    private var yVelocity = -20f
 
     private fun init() {
         setBackgroundColor(Color.RED)
+
+
     }
+
+
+    private lateinit var _dot: ImageView
+
+    fun setDot(dot: ImageView) {
+        _dot = dot
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        startTimer()
+    }
+
+    private fun startTimer() {
+        Timer("schedule", false).schedule(0, 20) {
+            moveDot()
+        }
+    }
+
+    private fun moveDot() {
+
+
+        var newX = _dot.x + xVelocity
+        var newY = _dot.y + yVelocity
+
+        val effectiveWidth = this.width - _dot.width
+        val effectiveHeight = this.height - _dot.height
+
+        if (newX < 0) {
+            newX = -newX
+            xVelocity = -xVelocity
+        }
+        if (newY < 0) {
+            newY = -newY
+            yVelocity = -yVelocity
+        }
+        if (newX > effectiveWidth) {
+            newX = 2 * effectiveWidth - newX
+            xVelocity = -xVelocity
+        }
+        if (newY > effectiveHeight) {
+            newY = 2 * effectiveHeight - newY
+            yVelocity = -yVelocity
+        }
+
+        _dot.x = newX
+        _dot.y = newY
+
+    }
+
 }
--- app/src/main/java/comandrenamatthiasz/httpsgithub/stayinside/MainActivity.kt
+++ app/src/main/java/comandrenamatthiasz/httpsgithub/stayinside/MainActivity.kt
@@ -1,42 +1,20 @@
 package comandrenamatthiasz.httpsgithub.stayinside
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.view.WindowManager
-import android.widget.ImageView
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
 
-    private var screenWidth = -1
-    private var screenHeight = -1
 
     val horizontalMode = "horizontal"
     val verticalMode = "vertical"
     private var mode = horizontalMode
 
-    private var xVelocity = -10f
-    private var yVelocity = -20f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val displayMetrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(displayMetrics)
-
-        screenWidth = displayMetrics.widthPixels
-        screenHeight = displayMetrics.heightPixels
-
-        setTextShown()
-        val timer = Timer("schedule", false).schedule(0,20){
-            moveDot()
-        }
-
-
 
         horizontalButton.setOnClickListener {
             mode = horizontalMode
@@ -46,37 +24,12 @@ class MainActivity : AppCompatActivity() {
             mode = verticalMode
             setTextShown()
         }
+
+        setTextShown()
+        gameView.setDot(dot)
     }
 
-    private fun moveDot() {
-        var newX = dot.x + xVelocity
-        var newY = dot.y + yVelocity
-        val effectiveWidth = screenWidth - dot.width
-        val effectiveHeight = screenHeight - dot.height
-        if(newX<0){
-            newX = -newX
-            xVelocity = -xVelocity
-        }
-        if(newY<0){
-            newY = -newY
-            yVelocity = -yVelocity
-        }
-        if(newX >effectiveWidth)
-        {
-            newX = 2*effectiveWidth-newX
-            xVelocity = -xVelocity
-        }
-        if(newY > effectiveHeight)
-        {
-            newY = 2*effectiveHeight-newY
-            yVelocity = -yVelocity
-        }
-
-        dot.x = newX
-        dot.y = newY
-    }
-
-    private fun setTextShown(){
-        textView.text=mode
+    private fun setTextShown() {
+        textView.text = mode
     }
 }
