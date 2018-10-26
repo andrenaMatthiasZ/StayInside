5298a40ec44cf1894fc4d7f9a12301bfa087e0c2
Matthias Zeleny <matthias.zeleny@andrena.de>
2018-10-26 10:27:42 +0200

    refactor code into named variables

--- app/src/main/java/comandrenamatthiasz/httpsgithub/stayinside/ViewExtension.kt
+++ app/src/main/java/comandrenamatthiasz/httpsgithub/stayinside/ViewExtension.kt
@@ -5,8 +5,13 @@ import android.view.View
 class ViewExtension{
     companion object {
         fun areOverlapping(first: View, second:View):Boolean{
-            val xOverlapping = first.x <= (second.x + second.width) && (first.x + first.width) >= second.x
-            val yOverlapping = first.y <= (second.y + second.height) && (first.y + first.height) >= second.y
+            val leftSideOfFirstIsLeftFromRightSightOfSecond = first.x <= (second.x + second.width)
+            val rightSideOfFirstIsRightFromLeftSideOfSecond = (first.x + first.width) >= second.x
+            val upperSideOfFirstIsAboveLowerSideOfSecond = first.y <= (second.y + second.height)
+            val lowerSideOfFirstIsBelowUpperSideOfSecond = (first.y + first.height) >= second.y
+
+            val xOverlapping = leftSideOfFirstIsLeftFromRightSightOfSecond && rightSideOfFirstIsRightFromLeftSideOfSecond
+            val yOverlapping = upperSideOfFirstIsAboveLowerSideOfSecond && lowerSideOfFirstIsBelowUpperSideOfSecond
 
             return xOverlapping && yOverlapping
         }
